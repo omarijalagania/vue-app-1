22aec346b41fa3427ec1b2890c4ad9031d773bb9
omari <omari.jalagania@gmail.com>
2022-10-02 22:53:42 +0400

    add tailwind css to login form

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <login-form @login="login"></login-form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginForm from "./components/LoginForm.vue"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: { LoginForm },
+  data() {
+    return {
+      credentials: {
+        email: "",
+        password: "",
+      },
+    }
+  },
+  methods: {
+    login(email, password) {
+      this.credentials.email = email
+      this.credentials.password = password
+      console.log(this.credentials)
+    },
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="h-screen flex justify-center items-center">
+    <form class="w-[70%] h-[60%] flex">
+      <div
+        class="w-[30%] flex justify-center items-center rounded-md bg-gradient-to-t from-red-400 to-red-600 shadow-md"
+      >
+        <h3 class="text-5xl text-white font-semibold tracking-widest">Login</h3>
+      </div>
+      <div class="w-[70%] flex flex-col space-y-10 justify-center items-center">
+        <div class="flex flex-col w-[70%]">
+          <label
+            class="text-2xl tracking-widest text-red-400 font-bold"
+            for="email"
+            >Username</label
+          >
+          <input
+            class="border-b-[2px] w-[70%] border-red-500 py-3 outline-none"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter Email"
+            v-model="email"
+          />
+        </div>
+        <div class="flex flex-col w-[70%]">
+          <label
+            class="text-2xl tracking-widest text-red-400 font-bold"
+            for="password"
+            >Password</label
+          >
+          <input
+            class="border-b-[2px] w-[70%] border-red-500 py-3 outline-none"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter Password"
+            v-model="password"
+          />
+        </div>
+
+        <div class="w-[70%]">
+          <button
+            class="bg-red-400 px-5 py-2 rounded-lg text-white text-2xl"
+            type="submit"
+            @click.prevent="login"
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login-form",
+  data() {
+    return {
+      email: "",
+      password: "",
+    }
+  },
+  emits: ["login"],
+
+  methods: {
+    login() {
+      this.$emit("login", this.email, this.password)
+    },
+  },
+}
+</script>
