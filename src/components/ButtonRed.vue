cb9552b15aad759dbda29030e95fabb5f6a4cfca
omari <omari.jalagania@gmail.com>
2022-10-08 20:39:36 +0400

    feat: add test fields

--- src/components/ButtonRed.vue
+++ src/components/ButtonRed.vue
@@ -2,7 +2,6 @@
   <button
     class="bg-red-400 w-full md:w-auto px-5 py-2 rounded-lg text-white text-2xl animate__animated animate__fadeInUp"
     type="submit"
-    @click.prevent="login"
   >
     <slot></slot>
   </button>
@@ -11,11 +10,5 @@
 <script>
 export default {
   name: "button-red",
-  props: {
-    login: {
-      type: Function,
-      required: true,
-    },
-  },
 }
 </script>
--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="h-screen flex justify-center px-2 md:p-0 md:items-center">
-    <form class="w-full md:w-[70%] md:h-[60%] block md:flex">
+    <form
+      @submit.prevent="submit"
+      class="w-full md:w-[70%] md:h-[60%] block md:flex"
+    >
       <div
         class="w-[30%] hidden md:flex z-20 justify-center items-center rounded-md bg-gradient-to-t from-red-400 to-red-600 shadow-md"
       >
@@ -24,9 +27,11 @@
             type="email"
             name="email"
             id="email"
+            @blur="validateEmail"
             placeholder="Enter Email"
             v-model="email"
           />
+          <p v-if="this.validateMyEmail === 'error'">Required</p>
         </div>
         <div class="flex flex-col w-full md:w-[70%]">
           <label
@@ -44,8 +49,37 @@
           />
         </div>
 
+        <div class="flex items-center w-full md:w-[70%] space-x-3">
+          <label
+            class="text-sm tracking-widest text-red-400 animate__animated animate__fadeIn"
+            for="checkbox"
+            >Remember me</label
+          >
+          <input
+            class="w-full outline-none animate__animated animate__fadeInLeft"
+            type="checkbox"
+            name="checkbox"
+            id="checkbox"
+            v-model="rememberMe"
+          />
+        </div>
+
+        <div class="md:w-[70%] space-x-3">
+          <label
+            class="text-sm tracking-widest text-red-400 animate__animated animate__fadeIn"
+            for="checkbox"
+            >Select item</label
+          >
+          <select v-model="selected">
+            <option disabled value="select">Select Item</option>
+            <option value="Google">Google</option>
+            <option value="Amazon">Amazon</option>
+            <option value="Apple">Apple</option>
+          </select>
+        </div>
+
         <div class="w-full md:w-[70%]">
-          <button-red :login="login"> Login </button-red>
+          <button-red> Login </button-red>
         </div>
       </div>
     </form>
@@ -62,11 +96,24 @@ export default {
     return {
       email: "",
       password: "",
+      rememberMe: false,
+      validateMyEmail: "pending",
+      selected: "select",
     }
   },
   methods: {
-    login() {
-      console.log(this.email, this.password)
+    submit() {
+      console.log(this.email, this.password, this.rememberMe, this.selected)
+    },
+
+    validateEmail() {
+      if (this.email === "") {
+        this.validateMyEmail = "error"
+      } else if (this.email.includes("@")) {
+        this.validateMyEmail = "success"
+      } else {
+        this.validateMyEmail = "pending"
+      }
     },
   },
 }
